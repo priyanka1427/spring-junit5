23c60942cad0ce59a1235db7131795b6ce83c3e2
Priyanka Gupta <priyankagupta1427@gmail.com>
2021-06-16 14:12:58 +0530

    Order BO Impl

--- src/main/java/com/iss/junit5/bo/OrderBOImpl.java
+++ src/main/java/com/iss/junit5/bo/OrderBOImpl.java
@@ -1,26 +1,66 @@
 package com.iss.junit5.bo;
 
+import java.sql.SQLException;
+
 import com.iss.junit5.bo.exception.BOException;
 import com.iss.junit5.order.dao.OrderDAO;
 import com.iss.junit5.order.dto.Order;
 
 public class OrderBOImpl implements OrderBO {
 	
-	OrderDAO dao;
+	private OrderDAO dao;
 	
 	@Override
 	public boolean placeOrder(Order order) throws BOException {
-		return false;
+		try {
+			int result = dao.create(order);
+			
+			if(result == 0) {
+				return false;
+			}
+		} catch (SQLException e) {
+			throw new BOException(e);
+		}
+		return true;
 	}
 
 	@Override
 	public boolean cancelOrder(int id) throws BOException {
-		return false;
+		try {
+			Order order = dao.read(id);
+			order.setStatus("Cancelled");
+			int result = dao.update(order);
+			
+			if(result == 0) {
+				return false;
+			}
+		}
+		catch(SQLException e) {
+			throw new BOException(e);
+		}
+		return true;
 	}
 
 	@Override
 	public boolean deleteOrder(int id) throws BOException {
-		return false;
+		try {
+			int result = dao.delete(id);
+			if(result == 0) {
+				return false;
+			}
+		}
+		catch(SQLException e) {
+			throw new BOException(e);
+		}
+		return true;
+	}
+
+	public OrderDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(OrderDAO dao) {
+		this.dao = dao;
 	}
 
 }
--- src/main/java/com/iss/junit5/bo/exception/BOException.java
+++ src/main/java/com/iss/junit5/bo/exception/BOException.java
@@ -1,5 +1,11 @@
 package com.iss.junit5.bo.exception;
 
+import java.sql.SQLException;
+
 public class BOException extends Exception {
+
+	public BOException(SQLException e) {
+		super(e);
+	}
 
 }
